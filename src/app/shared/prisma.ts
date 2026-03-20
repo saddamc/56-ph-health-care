75eb510f05a4483a4314756050f7e3219f5ad3c9
saddamc <saddam13bd@gmail.com>
2026-03-20 12:36:42 +0600

    user.service 57-02 end

--- src/app/shared/prisma.ts
+++ src/app/shared/prisma.ts
@@ -1,6 +1,8 @@
 
-
-
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
